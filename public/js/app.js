cce70fe9619d052f46e51a88a396b6fe5791f35f
aixzair <alex.lerosier@gmail.com>
2025-04-03 09:43:03 +0200

    add : details maps

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ import root from './../../app/root.js';
 fetch(`${root()}/pages/partials/header.html`)
 .then(response => response.text())
 .then(header => {
+    // TODO
     header = header.replaceAll('__PROFIL__', root() + '/pages/profil.html');
     header = header.replaceAll('__PROJECTS__', root() + '/pages/project/index.html');
     header = header.replaceAll('__CONTACT__', root() + '/pages/contact.html');
@@ -11,3 +12,15 @@ fetch(`${root()}/pages/partials/header.html`)
     document.getElementById('header').innerHTML = header;
 })
 .catch(error => console.error(error));
+
+// Load the footer
+const footerPrimary = document.getElementById('footer-primary');
+
+if (footerPrimary != undefined) {
+    fetch(`${root()}/pages/partials/footer-primary.html`)
+    .then(response => response.text())
+    .then(footer => {
+        footerPrimary.innerHTML = footer;
+    })
+    .catch(error => console.error(error));
+}
