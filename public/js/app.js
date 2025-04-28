442c032f01c6c8fa4c7e13acb18893ca7996ec74
Alexandre Lerosier <alex.lerosier@gmail.com>
2025-04-28 23:32:33 +0200

    feat : feed

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,7 @@ fetch(`${root()}/pages/partials/header.html`)
     header = header.replaceAll('__LOGIN__', root() + '/pages/connection/login.html');
     header = header.replaceAll('__HOME__', root() + '/index.html');
     header = header.replaceAll('__HISTORY__', root() + '/pages/routes/history.html');
+    header = header.replaceAll('__FEED__', root() + '/pages/friends/feed.html');
     header = header.replaceAll('__HELP__', root() + '/pages/others/emergencies.html');
 
     document.getElementById('header').innerHTML = header;
@@ -22,6 +23,7 @@ if (footerPrimary != undefined) {
     .then(footer => {
         footer = footer.replaceAll('__HOME__', root() + '/index.html');
         footer = footer.replaceAll('__HISTORY__', root() + '/pages/routes/history.html');
+        footer = footer.replaceAll('__FEED__', root() + '/pages/friends/feed.html');
         footer = footer.replaceAll('__HELP__', root() + '/pages/others/emergencies.html');
 
         footerPrimary.innerHTML = footer;
